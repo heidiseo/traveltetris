ed9fade6d980924622157690a02719b768407c11
Heidi Seo <heidiseo1@gmail.com>
2019-05-28 14:20:45 +0100

    making sample flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,8 +5,22 @@ class FlightsController < ApplicationController
   skip_before_action :authenticate_user!
   def api
     skip_authorization
-    url = "https://api.skypicker.com/flights?flyFrom=london&to=paris&date_from=18/06/2019&date_to=18/06/2019&return_from=20/06/2019&return_to=20/06/2019&max_stopovers=0&sort=price&asc=1&curr=GBP&partner=picky&dtime_from=19:00&dtime_to=00:00&ret_dtime_from=17:00&ret_dtime_to=20:00&limit=1"
-    flights = open(url).read
-    @flight_search = JSON.parse(flights)
+    url_one = "https://api.skypicker.com/flights?flyFrom=london&to=paris&date_from=14/06/2019&date_to=14/06/2019&return_from=17/06/2019&return_to=17/06/2019&max_stopovers=0&sort=price&asc=1&curr=GBP&partner=picky&dtime_from=19:00&dtime_to=00:00&ret_dtime_from=19:00&ret_dtime_to=22:00&limit=1"
+    flights_one = open(url_one).read
+    flight_search_one = JSON.parse(flights_one)
+
+    url_two = "https://api.skypicker.com/flights?flyFrom=london&to=paris&date_from=28/06/2019&date_to=28/06/2019&return_from=30/06/2019&return_to=30/06/2019&max_stopovers=0&sort=price&asc=1&curr=GBP&partner=picky&dtime_from=19:00&dtime_to=00:00&ret_dtime_from=19:00&ret_dtime_to=22:00&limit=1"
+    flights_two = open(url_two).read
+    flight_search_two = JSON.parse(flights_two)
+
+    url_three = "https://api.skypicker.com/flights?flyFrom=london&to=barcelona&date_from=14/06/2019&date_to=14/06/2019&return_from=17/06/2019&return_to=17/06/2019&max_stopovers=0&sort=price&asc=1&curr=GBP&partner=picky&dtime_from=19:00&dtime_to=00:00&ret_dtime_from=19:00&ret_dtime_to=22:00&limit=1"
+    flights_three = open(url_three).read
+    flight_search_three = JSON.parse(flights_three)
+
+    url_four = "https://api.skypicker.com/flights?flyFrom=london&to=barcelona&date_from=28/06/2019&date_to=28/06/2019&return_from=30/06/2019&return_to=30/06/2019&max_stopovers=0&sort=price&asc=1&curr=GBP&partner=picky&dtime_from=19:00&dtime_to=00:00&ret_dtime_from=19:00&ret_dtime_to=22:00&limit=1"
+    flights_four = open(url_four).read
+    flight_search_four = JSON.parse(flights_four)
+
+    @flight_search = [flight_search_one, flight_search_two, flight_search_three, flight_search_four]
   end
 end
