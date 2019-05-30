b6a675ab67bd36945db655e5b5737b5f596e62c3
Maria Antonia Rojas <marojaslondono@gmail.com>
2019-05-30 15:03:37 +0100

    bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,16 @@
 class BookingsController < ApplicationController
 
-  def traveller_info
+  def new
+  end
+
+  def create
+  end
+
+  def edit #edit_booking_path
+    # redirect_to confirmation page
+  end
+
+  def update
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'plans#new'
-  resources :bookings, only: [:traveller_info]
-
+  resources :bookings
   resources :plans do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :flights, only: [:index] do
