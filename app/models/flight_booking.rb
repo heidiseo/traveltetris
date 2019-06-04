38fdf465f95febe974cb83b108734510cae0c66b
Heidi Seo <heidiseo1@gmail.com>
2019-06-04 09:57:12 +0100

    setting up stripe

--- app/models/flight_booking.rb
+++ app/models/flight_booking.rb
@@ -1,5 +1,4 @@
 class FlightBooking < ApplicationRecord
   belongs_to :booking
   belongs_to :flight
-  validates :first_name_passenger, :last_name_passenger, :passport, :nationality, presence: true
 end
