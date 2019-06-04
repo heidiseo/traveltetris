acabd1ef4db99391eb2d49d031be88481ef1e12b
omololaibojo <lola.polly@yahoo.it>
2019-06-04 11:43:44 +0100

    Payment model and booking show action

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :plan
   has_many :flight_bookings, dependent: :destroy
-  validates :first_name_passenger, :last_name_passenger, :passport, :nationality, presence: true
-  monetize :price_cents
+  # validates :first_name_passenger, :last_name_passenger, :passport, :nationality, presence: true
+  monetize :amount_cents
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'plans#new'
-  resources :bookings
+  resources :bookings do
+    resources :payments, only: [:create]
+  end
 
   resources :plans do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
